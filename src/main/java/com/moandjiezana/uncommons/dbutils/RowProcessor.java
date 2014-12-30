b20ed9294afea707df6aa8c7d56b51b5740cb08d
moandji.ezana <mwanji@gmail.com>
2014-12-30 10:29:59 +0200

    Added RowProcessor#optional() and javadoc

--- src/main/java/com/moandjiezana/uncommons/dbutils/RowProcessor.java
+++ src/main/java/com/moandjiezana/uncommons/dbutils/RowProcessor.java
@@ -1,13 +1,45 @@
 package com.moandjiezana.uncommons.dbutils;
 
 import java.sql.ResultSet;
+import java.util.Optional;
 
+/**
+ * Converts a {@link ResultSet} to an instance of T
+ * 
+ * @param <T>
+ *    the type a row is converted to
+ */
 @FunctionalInterface
 public interface RowProcessor<T> {
 
+  /**
+   * @param resultSet
+   *    already pointing to the row to be processed
+   * @return
+   *    an instance of T
+   * @throws Exception
+   *    if something goes wrong
+   */
   T handle(ResultSet resultSet) throws Exception;
 
+  /**
+   * @return
+   *    {@link RowProcessor} that gets the first column of the row
+   * @see ColumnRowProcessor
+   */
   static <T> RowProcessor<T> firstColumn() {
     return new ColumnRowProcessor<T>(1);
+  }
+
+  /**
+   * @param rowProcessor
+   *    performs the processing
+   * @param <T>
+   *    the type a row is converted to
+   * @return
+   *    the result of processing, wrapped in an {@link Optional}
+   */
+  static <T> RowProcessor<Optional<T>> optional(RowProcessor<T> rowProcessor) {
+    return rs -> Optional.ofNullable(rowProcessor.handle(rs));
   }
 }
